d933318d59ba73b44c159be4d9c56a45dce712df
Varun Dunna <95626468+bloxabyte@users.noreply.github.com>
2022-06-15 20:43:49 +0530

    Update Arrays_1.java

--- Arrays_1.java
+++ Arrays_1.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Arrays_1 {
@@ -9,27 +10,33 @@ public class Arrays_1 {
         length = in.nextInt();
         if ((length<=1000)&(length>=1)){
             int[] nums = new int[length];
-
+            int cnt = 0;//counter for value 0
             for (int i = 0; i<length ; i++){
-                System.out.print("Enter the element to be appended to the array : ");
-                int element = in.nextInt();
-                for (int j =0 ;j< nums.length;j++){
-                    if (nums[j]!=element){
-                        nums[j] = element;
+                    System.out.print("Enter the element to be appended to the array : ");
+                    int element = in.nextInt();
+                    if (0==element&(i==0)){
+                    nums[i] = element;
+                    cnt = 1;
+            }
+                    else if(cnt==0&nums[i]==0){   // here when nums[i]=0 , the RHS is the value of element
+                        nums[i] = element;
                     }
+                    else if(nums[i]!=element){
+                        nums[i] = element;
+                }
                     else{
-                        System.out.println("You have entered a number that already exists in the array. enter the number again.");
-                        j-=1;
-                        break;
+                        System.out.println("You have entered a number that already exists in the array . Try again.");
+                        i--;
+
                     }
                 }
-            }
+
             int[] ans = new int[length] ;
             for (int j = 0;j<nums.length;j++){
                 ans[j]=nums[nums[j]];
             }
             System.out.print("Required array is : ");
-            System.out.println(ans);
+            System.out.println(Arrays.toString(ans));
 
         }else{
             System.out.println("Invalid length of array specified.");
